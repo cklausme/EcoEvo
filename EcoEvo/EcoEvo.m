ac074f0f369ea6e8557b46741fdf7ace636e610f
cklausme <klausme1@msu.edu>
2019-09-04 16:31:51 -0400

    date

--- EcoEvo/EcoEvo.m
+++ EcoEvo/EcoEvo.m
@@ -256,7 +256,7 @@ ZeroDiagonal::usage =  "ZeroDiagonal is an option for PlotPIP that forces Inv=0 
 Begin["`Private`"];
 
 
-$EcoEvoVersion="1.0.0 (August 13, 2019)";
+$EcoEvoVersion="1.0.0 (September 4, 2019)";
 
 
 modelloaded=False;
