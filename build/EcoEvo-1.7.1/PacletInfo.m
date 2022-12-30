8ab62592d29708cb588dca0ccdec72f59c063f75
cklausme <klausme1@msu.edu>
2022-12-29 22:01:11 -0800

    1.7.1 built

---
+++ build/EcoEvo-1.7.1/PacletInfo.m
@@ -0,0 +1,21 @@
+(* Paclet Info File *)
+
+(* created 2022/12/29*)
+
+Paclet[
+    Name -> "EcoEvo",
+    Version -> "1.7.1",
+    MathematicaVersion -> "10+",
+    Description -> "Species- and trait-based ecological and eco-evolutionary modeling.",
+    Creator -> "Christopher Klausmeier <christopher.klausmeier@gmail.com>",
+    Thumbnail -> "Logo.png",
+    Root -> "EcoEvo",
+    Icon -> "Logo.png",
+    Extensions -> 
+        {
+            {"Documentation", MainPage -> "Guides/EcoEvo", Language -> All}, 
+            {"Kernel", Root -> ".", Context -> "EcoEvo`"}
+        }
+]
+
+
